2ffe53343135f97f0818cf08e4bec1c5e1cb7411
Mamoun Hisham <mamounprogrammer@gmail.com>
2025-01-27 16:46:02 +0200

    start on filtring

--- app/Http/Controllers/Api/V1/DoctorController.php
+++ app/Http/Controllers/Api/V1/DoctorController.php
@@ -7,15 +7,43 @@ use App\Http\Requests\StoreDoctorRequest;
 use App\Http\Requests\UpdateDoctorRequest;
 use App\Http\Resources\Api\V1\DoctorResource;
 use App\Models\Doctor;
+use Illuminate\Http\Request;
+use Spatie\QueryBuilder\AllowedFilter;
+use Spatie\QueryBuilder\QueryBuilder;
+use Spatie\QueryBuilder\Enums\FilterOperator;
 
 class DoctorController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        return DoctorResource::collection(Doctor::paginate());
+        $doctors = QueryBuilder::for(Doctor::class)
+            ->allowedIncludes(['user'])
+            ->allowedFilters([
+                'speciality',
+                'qualification',
+                'bio',
+                'user.name',
+                'user.city',
+                'user.state',
+                'user.country',
+                AllowedFilter::exact('status'),
+                AllowedFilter::exact('available_days'),
+            ])
+            ->allowedSorts([
+                'speciality',
+                'qualification',
+                'experience',
+                'consultation_fee',
+                'status',
+                'created_at'
+            ])
+            ->paginate()
+            ->appends($request->query());
+
+        return DoctorResource::collection($doctors);
     }
 
     /**
